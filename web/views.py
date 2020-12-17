1ed73c4824b3527f3fa0e39e2f6f35ffffb82d7c
H10113 <gtseng0218@gmail.com>
2020-12-17 10:48:14 +0800

    新增專案、路徑規則、處理視圖

---
+++ web/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.views.generic import  * #ListView, DetailView, CreateView
+from django.urls import reverse_lazy
+from .models import  * #Message
+
+# Create your views here.
+class MessageList(ListView):
+    model = Message
+
+class MessageDetail(DetailView):
+    model = Message
+
+class MessageCreate(CreateView):
+    model = Message
+    #fields = ['user','subject','content']
+    fields = '__all__'
+    success_url = reverse_lazy('msg_list')
